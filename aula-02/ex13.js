0d89ed66794078a63bd48a42f66d7590c6595559
Iarly Gabriel de Araújo <wyndhoply@gmail.com>
2026-06-16 19:06:39 -0300

    fix(): Corrigido erro fatal no código de raiz quadrada

--- aula-02/ex13.js
+++ aula-02/ex13.js
@@ -2,12 +2,12 @@ let numA = 9
 let quadrado = numA ** 2;
 
 let numB = 0;
-let adivinha = numB * numB
+let adivinha
 while (adivinha != numA) {
     numB = numB + 1 
-    adivinha = numB 
+    adivinha = numB * numB
 }
 
 console.log(`seu numero é ${numA}`)
 console.log(`o quadrado dele é ${quadrado}`)
-console.log(`a raiz dele é ${adivinha}`)
+console.log(`a raiz dele é ${numB}`)
