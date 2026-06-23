8714300d699351caa2c22b3abf379c0e4f32efa0
Wynd-Hoply <wyndhoply@gmail.com>
2026-06-23 11:38:22 -0300

    chamar o joao

---
+++ aula07/exemplo01.js
@@ -0,0 +1,16 @@
+let aluno = {
+    nome: "João",
+    idade: 18,
+    endereco: {
+        logradouro:"Rua A",
+        numero:10,
+        complemento:"Apto 401",
+        bairro:"Posse",
+        cidade:"Velha Iguaçu",
+        uf:"RJ",
+        cep: "26023-876"
+    }
+}
+
+console.log(aluno.nome)
+console.log(aluno.endereco.cidade);
